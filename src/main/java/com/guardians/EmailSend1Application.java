6f031c21115b8d97e2198dfe31e0f5e987d604df
iamkiranrajput <rajputkiran2805@gmail.com>
2024-06-29 22:05:18 +0530

    first commit

---
+++ src/main/java/com/guardians/EmailSend1Application.java
@@ -0,0 +1,13 @@
+package com.guardians;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSend1Application.class, args);
+	}
+
+}
