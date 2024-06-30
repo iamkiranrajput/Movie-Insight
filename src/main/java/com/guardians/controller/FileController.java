2a8a8a6baa5e081c50854c683de7614584217582
iamkiranrajput <rajputkiran2805@gmail.com>
2024-06-30 09:38:16 +0530

    file Controller

--- src/main/java/com/guardians/controller/FileController.java
+++ src/main/java/com/guardians/controller/FileController.java
@@ -2,23 +2,27 @@ package com.guardians.controller;
 
 
 import com.guardians.service.FileService;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 @RequestMapping("/file/")
 public class FileController {
+    @Autowired
     private final FileService fileService;
-
-
     public FileController(FileService fileService) {
         this.fileService = fileService;
     }
@@ -29,6 +33,14 @@ public class FileController {
     @PostMapping("/upload")
     public ResponseEntity<String> uploadFileHandler(@RequestPart MultipartFile file) throws IOException{
         String uploadedFileName = fileService.uploadFile(path,file);
-        return ResponseEntity.ok("File Uploaded"+uploadedFileName);
+        return ResponseEntity.ok("File Uploaded "+uploadedFileName);
     }
+
+    @GetMapping("/{fileName}")
+    public void serveFileHandler(@PathVariable String fileName, HttpServletResponse response) throws IOException {
+        InputStream resourceFile = fileService.getResourceFile(path,fileName);
+        response.setContentType(MediaType.IMAGE_PNG_VALUE);
+        StreamUtils.copy(resourceFile,response.getOutputStream());
+    }
+
 }
