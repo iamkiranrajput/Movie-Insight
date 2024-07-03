cc6b8239ef9864e800fb6b5262b2c902ccf2f6e7
iamkiranrajput <rajputkiran2805@gmail.com>
2024-07-03 11:49:51 +0530

    put mapping deleting and getting all the movies data

--- src/main/java/com/guardians/controller/MovieController.java
+++ src/main/java/com/guardians/controller/MovieController.java
@@ -11,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/movie")
@@ -29,6 +31,29 @@ public class MovieController {
         ObjectMapper objectMapper= new ObjectMapper();
         MovieDto movieDto = objectMapper.readValue(movieDtoObj, MovieDto.class);
         return  movieDto;
+    }
+
+
+    @GetMapping("/{movieId}")
+    public ResponseEntity<MovieDto> getMovieHandler(@PathVariable Integer movieId){
+        return ResponseEntity.ok(movieService.getMovie(movieId));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<MovieDto>> getAllMovieHandler(){
+        return ResponseEntity.ok(movieService.getAllMovies());
+    }
+    @PutMapping("/update/{movieId}")
+    public ResponseEntity<MovieDto> updateMovieHandler(@PathVariable Integer movieId, @RequestPart MultipartFile file, @RequestPart String movieDtoObj) throws IOException {
+
+        if(file.isEmpty())file = null;
+        MovieDto movieDto = convertToMovieDto(movieDtoObj);
+        return ResponseEntity.ok((movieService.updateMovie(movieId,movieDto,file)));
+    }
+
+    @DeleteMapping("/delete/{movieId}")
+    public ResponseEntity<String> deleteMovieHandler(@PathVariable Integer movieId) throws IOException {
+        return ResponseEntity.ok(movieService.deleteMovie(movieId));
     }
 
 }
