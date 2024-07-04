83a2cd50036e78eca57e0831deaf0fdc121b33db
iamkiranrajput <rajputkiran2805@gmail.com>
2024-07-04 15:47:36 +0530

    handling Internal server Error Exception

--- src/main/java/com/guardians/exception/GlobalExceptionHandler.java
+++ src/main/java/com/guardians/exception/GlobalExceptionHandler.java
@@ -1,15 +1,16 @@
 package com.guardians.exception;
 
 
+import org.springframework.data.mapping.PropertyReferenceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.io.IOException;
-
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(MovieNotFoundException.class)
@@ -20,25 +21,26 @@ public class GlobalExceptionHandler {
     public ProblemDetail handleFileExistsException(FileExistsException ex){
         return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
     }
-
     @ExceptionHandler(EmptyFileException.class)
     public ProblemDetail handleEmptyFileException(EmptyFileException ex){
         return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
     }
-
-    @ExceptionHandler(IOException.class)
-    public ProblemDetail handleIOException(IOException ex){
-        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ProblemDetail handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex){
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Kindly enter valid ID");
     }
-
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ProblemDetail handleHttpNoResourceFoundException(NoResourceFoundException ex){
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, "Page not Found");
+    }
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ProblemDetail handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException ex){
         return ProblemDetail.forStatusAndDetail(HttpStatus.METHOD_NOT_ALLOWED, ex.getMessage());
     }
 
-    @ExceptionHandler(NoResourceFoundException.class)
-    public ProblemDetail handleHttpNoResourceFoundException(NoResourceFoundException ex){
-        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+    @ExceptionHandler(PropertyReferenceException.class)
+    public ProblemDetail handlePropertyReferenceException(PropertyReferenceException ex){
+        return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
     }
 
 
