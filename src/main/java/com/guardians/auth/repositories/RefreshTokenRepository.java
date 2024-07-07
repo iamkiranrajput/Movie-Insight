98de3f8156b6e2d173ffc0e4d4dbe65b8229354c
iamkiranrajput <rajputkiran2805@gmail.com>
2024-07-07 19:32:13 +0530

    JWT Authentication

---
+++ src/main/java/com/guardians/auth/repositories/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package com.guardians.auth.repositories;
+
+import com.guardians.auth.entities.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken,Integer> {
+
+
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
